32f4c416c821026118da684f882f6b0e2ac02d34
plisa1 <plisaxx@gmail.com>
2023-08-12 23:27:17 -0400

    "assignment 6 done"

--- src/components/Buttons.js
+++ src/components/Buttons.js
@@ -1,10 +1,23 @@
-const Buttons = () => {
+import satData from "./satData";
+import "./styling.css";
+
+function Buttons({ filterByType, setSat, displaySats }) {
   return (
-    <div>
-      <button>Placeholder Button</button>
-      <button>All Orbits</button>
+      <div className="flex-container">
+          {displaySats.map((sat, id) => {
+              return (
+                  <button onClick={() => filterByType(sat)} key={id}>
+                      {sat} Orbit
+                  </button>
+              );
+          })}
+
+          {/* Second button */}
+          <button onClick={() => setSat(satData)}>
+              Show All Satellites
+          </button>
       </div>
   );
-};
+}
 
 export default Buttons;
